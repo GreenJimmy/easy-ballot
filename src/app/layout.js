27fac167a86991cd3cdfd5f58f07f86fe035a5c0
JT <jathompson@mdsol.com>
2024-09-06 20:26:37 -0700

    Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,28 @@
 import Head from "next/head";
-import { Container, Row, Col } from "react-bootstrap";
+import { Antonio, Montserrat } from "next/font/google";
+import {
+  Container,
+  Navbar,
+  NavbarBrand,
+  NavbarToggle,
+  NavbarCollapse,
+  Nav,
+  NavLink,
+} from "react-bootstrap";
 
 import "../styles/index.scss";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
+});
+
+const antonio = Antonio({
+  subsets: ["latin"],
+  variable: "--font-antonio",
+  display: "swap",
+});
 
 export const viewport = {
   width: "device-width",
@@ -28,23 +49,29 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/img/logo.svg" />
         <title>Easy Ballot</title>
       </Head>
-      <html lang="en">
+      <html lang="en" className={`${montserrat.variable} ${antonio.variable}`}>
         <body>
-          <Container id="main-container">
-            <Row>
-              <Col>
-                <a class="logo" href="/">
-                  <img src="/img/logo.svg" />
+          <Container id="main-container" fluid className="g-0">
+            <Navbar variant="dark">
+              <Container>
+                <NavbarBrand href="/" className="d-flex align-items-center">
+                  <img src="/img/logo.svg" className="me-3" />
                   <span property="name">EasyBallot.vote</span>
-                </a>
-              </Col>
-              <Col>
-                <a href="mailto:info@downballot.app" class="contact-us">
-                  Contact Us
-                </a>
-              </Col>
-            </Row>
-            {children}
+                </NavbarBrand>
+                <NavbarToggle />
+                <NavbarCollapse className="justify-content-end">
+                  <Nav className="ms-auto">
+                    <NavLink href="#home">Home</NavLink>
+                    <NavLink href="mailto:info@downballot.app">
+                      Contact Us
+                    </NavLink>
+                  </Nav>
+                </NavbarCollapse>
+              </Container>
+            </Navbar>
+            <Container id="content" fluid>
+              <Container>{children}</Container>
+            </Container>
           </Container>
         </body>
       </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,20 @@
+import { Row, Col } from "react-bootstrap";
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <Row>
+        <Col className="three-stock-photos">
+          <img className="stock-l" src="/img/stock_l.jpg" />
+          <img src="/img/stock_c.jpg" />
+          <img className="stock-r" src="/img/stock_r.jpg" />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <h2>HELPING FRIENDS VOTE WITH CONFIDENCE</h2>
+        </Col>
+      </Row>
+    </>
+  );
 }
