76471a49002f104f5b61ba03fa623814897a5fe7
JT <jathompson@mdsol.com>
2024-09-07 16:31:57 -0700

    Finalize landing page layout and css

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,15 @@ import Head from "next/head";
 import { Antonio, Montserrat } from "next/font/google";
 import Link from "next/link";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
 import NavbarToggle from "react-bootstrap/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 import Nav from "react-bootstrap/Nav";
 import NavLink from "react-bootstrap/NavLink";
+import Button from "react-bootstrap/Button";
 
 import "../styles/index.scss";
 
@@ -37,7 +40,7 @@ export default function RootLayout({ children }) {
       <html lang="en" className={`${montserrat.variable} ${antonio.variable}`}>
         <body>
           <Container id="main-container" fluid className="g-0">
-            <Navbar variant="dark">
+            <Navbar expand="sm" variant="dark">
               <Container>
                 <NavbarBrand
                   href="/"
@@ -50,9 +53,6 @@ export default function RootLayout({ children }) {
                 <NavbarToggle />
                 <NavbarCollapse className="justify-content-end">
                   <Nav className="ms-auto">
-                    <NavLink href="/" as={Link}>
-                      Home
-                    </NavLink>
                     <NavLink href="mailto:info@downballot.app">
                       Contact Us
                     </NavLink>
@@ -61,7 +61,47 @@ export default function RootLayout({ children }) {
               </Container>
             </Navbar>
             <Container id="content" fluid>
-              <Container>{children}</Container>
+              <Container>
+                {children}
+                <Row>
+                  <Col className="mt-5"></Col>
+                </Row>
+              </Container>
+            </Container>
+            <Container fluid id="footer" className="py-2 py-md-4">
+              <Container>
+                <Row>
+                  <Col md="3" lg="2" className="d-flex align-items-center my-2">
+                    <Nav className="flex-column mx-auto mx-md-0 me-md-auto">
+                      <NavLink href="mailto:info@downballot.app">
+                        Contact Us
+                      </NavLink>
+                    </Nav>
+                  </Col>
+                  <Col
+                    md="6"
+                    lg="8"
+                    className="text-center d-flex-align-items-center my-2"
+                  >
+                    <Link href="/">
+                      <img
+                        src="/img/logo-button.svg"
+                        className="w-100 mx-auto"
+                        style={{ maxWidth: "300px" }}
+                      />
+                    </Link>
+                  </Col>
+                  <Col md="3" lg="2" className="d-flex align-items-center my-2">
+                    <Button
+                      variant="secondary"
+                      className="mx-auto mx-md-0 ms-md-auto"
+                      size="lg"
+                    >
+                      Donate
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
             </Container>
           </Container>
         </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
@@ -5,66 +6,124 @@ import Button from "react-bootstrap/Button";
 export default function Home() {
   return (
     <>
-      <Row>
-        <Col className="three-stock-photos">
-          <img className="stock-l" src="/img/stock_l.jpg" />
-          <img src="/img/stock_c.jpg" />
-          <img className="stock-r" src="/img/stock_r.jpg" />
+      <Row className="my-5">
+        <Col xs="0" lg="3" className="align-items-center d-none d-lg-flex">
+          <img src="/img/stock_l.jpg" className="w-100" />
         </Col>
+        <Col xs="12" lg="6" className="d-flex align-items-center">
+          <img src="/img/stock_c.jpg" className="w-100" />
+        </Col>
+        <Col lg="3" className="align-items-center d-none d-lg-flex">
+          <img src="/img/stock_r.jpg" className="w-100" />
+        </Col>
+      </Row>
+      <Row>
+        <Col lg="2" xl="3"></Col>
+        <Col lg="8" xl="6" className="text-center">
+          <h2 className="mb-5">Helping Friends Vote with Confidence</h2>
+        </Col>
+        <Col lg="2" xl="3"></Col>
+      </Row>
+      <Row>
+        <Col md="2" lg="3" xxl="4" className="d-flex align-items-center">
+          <hr className="w-100" />
+        </Col>
+        <Col md="8" lg="6" xxl="4" className="text-center">
+          <Link href="#how-it-works">
+            <Button size="lg" className="m-2 m-sm-3">
+              Learn More
+            </Button>
+          </Link>
+          <Link href="/get-started">
+            <Button size="lg" className="m-2 m-sm-3">
+              Get Started
+            </Button>
+          </Link>
+        </Col>
+        <Col md="2" lg="3" xxl="4" className="d-flex align-items-center">
+          <hr className="w-100" />
+        </Col>
+      </Row>
+      <Row>
+        <Col md="1" xl="2" />
+        <Col md="10" xl="8">
+          <div className="box-borders my-5 py-md-5">
+            <Row>
+              <Col
+                lg="6"
+                xxl="4"
+                className="d-flex align-items-center text-center mb-4 mb-lg-0"
+              >
+                <h1 className="m-0" id="how-it-works">
+                  HOW IT WORKS
+                </h1>
+              </Col>
+              <Col lg="6" xxl="8" className="d-flex align-items-center">
+                <img src="/img/how-it-works.jpg" className="w-100" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p className="mt-5">
+                  Do your friends call you asking how they should fill out their
+                  ballot this year?
+                </p>
+                <p>
+                  <strong>We thought so.</strong>
+                </p>
+                <p>
+                  Well, we're here to make your life a whole lot easier.
+                  EasyBallot.vote is a tool that helps you create a customized
+                  ballot for any and all races you want to weigh in on across
+                  the state. Once you have made your ballot, you can share the
+                  link with your friends. By entering their address, they will
+                  have access to their specific ballot with your recommendations
+                  on important races and propositions.
+                </p>
+                <p className="mb-0">
+                  Are you ready to make a huge impact on this election by
+                  helping your friends vote up and down their ballots?
+                </p>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg="4" className="text-center mt-5">
+                <img src="/img/create.svg" style={{ height: "10rem" }} />
+                <h3 className="my-3">Step 1</h3>
+                <p className="mb-0 mx-auto" style={{ maxWidth: "20rem" }}>
+                  Create your customized ballot.
+                </p>
+              </Col>
+              <Col lg="4" className="text-center mt-5 step-2">
+                <img src="/img/share.svg" style={{ height: "10rem" }} />
+                <h3 className="my-3">Step 2</h3>
+                <p className="mb-0 mx-auto" style={{ maxWidth: "20rem" }}>
+                  Share your ballot link with your friends and family.
+                </p>
+              </Col>
+              <Col lg="4" className="text-center mt-5 step-3">
+                <img src="/img/win.svg" style={{ height: "10rem" }} />
+                <h3 className="my-3">Step 3</h3>
+                <p className="mb-0 mx-auto" style={{ maxWidth: "20rem" }}>
+                  Sit back and watch your favorite candidates win all across the
+                  state!
+                </p>
+              </Col>
+            </Row>
+            <div className="box-tl d-none d-md-block" />
+            <div className="box-tr d-none d-md-block" />
+            <div className="box-bl d-none d-md-block" />
+            <div className="box-br d-none d-md-block" />
+          </div>
+        </Col>
+        <Col md="1" xl="2" />
       </Row>
       <Row>
         <Col className="text-center">
-          <h2 className="mb-5">Helping Friends Vote with Confidence</h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col md="3" className="d-flex align-items-center">
-          <hr className="w-100" />
-        </Col>
-        <Col md="6" className="text-center">
-          <Button size="lg" className="m-3">
-            Learn More
-          </Button>
-          <Button size="lg" className="m-3">
-            Get Started
-          </Button>
-        </Col>
-        <Col md="3" className="d-flex align-items-center">
-          <hr className="w-100" />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <div className="m-5 p-5 box-borders">
-            <div className="how-it-works mb-4">
-              <h1 className="m-0">HOW IT WORKS</h1>
-              <img src="/img/how-it-works.jpg" />
-            </div>
-            <p>
-              Do your friends call you asking how they should fill out their
-              ballot this year?
-            </p>
-            <p>
-              <strong>We thought so.</strong>
-            </p>
-            <p>
-              Well, we're here to make your life a whole lot easier.
-              EasyBallot.vote is a tool that helps you create a customized
-              ballot for any and all races you want to weigh in on across the
-              state. Once you have made your ballot, you can share the link with
-              your friends. By entering their address, they will have access to
-              their specific ballot with your recommendations on important races
-              and propositions.
-            </p>
-            <p>
-              Are you ready to make a huge impact on this election by helping
-              your friends vote up and down their ballots?
-            </p>
-            <div className="box-tl" />
-            <div className="box-tr" />
-            <div className="box-bl" />
-            <div className="box-br" />
-          </div>
+          <h2 className="mb-4">Let's Get Started!</h2>
+          <Link href="/get-started">
+            <Button size="lg">Get Started</Button>
+          </Link>
         </Col>
       </Row>
     </>
