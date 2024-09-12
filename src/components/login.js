174a03a4972684df2f1b77f197d0d921923fe2cc
JT <jathompson@mdsol.com>
2024-09-12 09:34:54 -0700

    Get cookie

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,25 @@
 "use client";
 
 import { GoogleLogin } from "@react-oauth/google";
+import { useRouter } from "next/navigation";
+import { toFormData } from "../scripts/utils";
 
 const Login = () => {
+  const router = useRouter();
   return (
     <GoogleLogin
-      onSuccess={(credentialResponse) => {
-        console.log(credentialResponse);
-        sessionStorage.setItem("gAuth", JSON.stringify(credentialResponse));
+      onSuccess={async (credentialResponse) => {
+        const response = await fetch(
+          `https://${process.env.NEXT_PUBLIC_API_URL}/auth/google`,
+          {
+            method: "POST",
+            body: toFormData({
+              credential: credentialResponse.credential,
+            }),
+          }
+        );
+
+        router.push("/create");
       }}
       onError={() => {
         console.log("Login Failed");
